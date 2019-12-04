5fe76db19093eeff1dadeb4ac88b4b53b8ed8259
Psycr0w <ghost@arau.at>
2019-12-04 16:16:34 +0100

    MoveFilesFromFolder, WeekdayCalculations

---
+++ MoveFilesFromFolder.ps1
@@ -0,0 +1,24 @@
+﻿#Variablendeklaration
+$ordner = Get-ChildItem -Directory
+$haupt = Get-Location
+
+#Hauptverarbeitung
+
+foreach ($i in $ordner)
+{
+    $a = Get-Random
+    $str = [string]$a 
+    Rename-Item $i -NewName $str
+}
+
+#Variablendeklaration
+$ordner = Get-ChildItem -Directory
+
+#Hauptverarbeitung
+
+foreach ($i in $ordner) 
+{
+    $a = Get-ChildItem $i
+    Move-Item ($a.FullName) -Destination $haupt
+    Remove-Item $i
+}
---
+++ WeekdayCalculations.ps1
@@ -0,0 +1,11 @@
+﻿$startdate = Get-Date -Date '2019-05-01'
+$enddate = Get-Date -Date '2019-12-20'
+
+$difference = New-TimeSpan -Start $startdate -End $enddate
+$days = [Math]::Ceiling($difference.TotalDays)+1
+
+1..$days | ForEach-Object {
+  $startdate
+  $startdate = $startdate.AddDays(1)
+} |
+  (Where-Object { $_.DayOfWeek -gt 0 -and $_.DayOfWeek -le 2} | Select-Object -Property Date).count()
