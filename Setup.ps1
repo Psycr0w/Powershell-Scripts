8813925e624e6dd5f2f64407ee02aae928eaceda
Psycr0w <ghost@arau.at>
2020-02-24 20:08:18 +0100

    Create Setup.ps1

---
+++ Setup.ps1
@@ -0,0 +1,24 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+
+#Utilities
+choco install sumatrapdf
+choco install discord
+choco install firefox
+choco install 7zip.install
+choco install notepadplusplus.install
+choco install mumble
+choco install cccp
+choco install mpc-hc
+
+#Programming
+choco install git
+choco install visualstudio2019community
+choco install vscode
+
+#Organisation
+choco install joplin
+choco install cherrytree
+
+#Gaming
+choco install steam
