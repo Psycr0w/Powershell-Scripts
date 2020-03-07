a081c950f014370c90e029514dba41e48b043a9e
Psycr0w <51051984+Psycr0w@users.noreply.github.com>
2020-03-07 20:33:07 +0100

    Update Setup.ps1
    Setup Update

--- Setup.ps1
+++ Setup.ps1
@@ -2,32 +2,32 @@
 
 
 #Utilities
-choco install sumatrapdf
-choco install discord
-choco install firefox
-choco install 7zip.install
-choco install notepadplusplus.install
-choco install mumble
-choco install cccp
-choco install mpc-be
-choco install irfanview
-choco install mremoteng
-choco install curl
-choco install dropbox
-choco install windirstat
-choco install greenshot
+choco install sumatrapdf -y
+choco install discord -y
+choco install firefox -y
+choco install 7zip.install -y
+choco install notepadplusplus.install -y
+choco install mumble -y
+choco install cccp -y
+choco install mpc-be -y
+choco install irfanview -y
+choco install mremoteng -y
+choco install curl -y
+choco install dropbox -y
+choco install windirstat -y
+choco install greenshot -y
 
 #Programming
-choco install git
-choco install visualstudio2019community
-choco install vscode
+choco install git -y
+choco install visualstudio2019community -y
+choco install vscode -y
 
 #Organisation
-choco install joplin
-choco install cherrytree
+choco install joplin -y
+choco install cherrytree -y
 
 #Gaming
-choco install steam
+choco install steam -y
 
 #Requirements
-choco install javaruntime
+choco install javaruntime -y
