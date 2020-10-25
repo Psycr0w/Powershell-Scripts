ce51ff96f8fbbaeff8553f1b1913c049301df30c
Psycr0w <51051984+Psycr0w@users.noreply.github.com>
2020-10-25 20:51:27 +0100

    Add files via upload
    WowItemId extraction via wowhead

---
+++ ListWoWItemsWithIDbyAddon.ps1
@@ -0,0 +1,22 @@
+﻿$listwowAddons =
+"Classic",
+"The Burning Crusade",
+"Wrath of the Lich King",
+"Cataclysm",
+"Mists of Pandaris",
+"Warlords of Draenor",
+"Legion",
+"Battle for Azeroth",
+"Shadowlands"
+
+[regex]$rx = '(?<=")\d+(?=":{"name_)'
+
+for ($i=0; $i -lt $listwowAddons.Count; $i++)
+{
+    $index = $i + 1
+    $a = Invoke-WebRequest "https://de.wowhead.com/items?filter=166;$index;0"
+
+    $content = $a.RawContent
+    $name = $listwowAddons[$i]
+    $rx.Matches($content).Value | Out-File "$env:USERPROFILE\Documents\WarcraftItems\$name.txt"
+}
