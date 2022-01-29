d78f1803b77f67394120639e9651061a0461ca16
Andreas Rauch <51051984+Psycr0w@users.noreply.github.com>
2022-01-29 09:08:20 +0100

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -1,21 +1,25 @@
-﻿Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 
 #Utilities
 choco install sumatrapdf.install -y
-choco install discord -y
 choco install firefox -y
 choco install 7zip.install -y
 choco install notepadplusplus.install -y
-choco install mumble -y
 choco install cccp -y
 choco install mpc-be -y
 choco install irfanview -y
 choco install mremoteng -y
+choco install nextcloud-client -y
 choco install curl -y
 choco install dropbox -y
 choco install windirstat -y
 choco install greenshot -y
+choco install steelseries-engine -y
+choco install filezilla -y
+choco install windirstat -y
+choco install winscp -y
+choco install tabby -y
 
 #Programming
 choco install git -y
@@ -32,3 +36,11 @@ choco install steam -y
 
 #Requirements
 choco install javaruntime -y
+
+#graphics
+choco install geforce-game-ready-driver -y
+
+#Comms
+choco install webex -y
+choco install discord -y
+choco install mumble -y
