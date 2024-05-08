c5565d42aee1289ccfeda2c8ff58d81cb844bea5
Andreas Rauch <51051984+Psycr0w@users.noreply.github.com>
2024-05-08 22:57:12 +0100

    Update Setup.ps1

--- Setup.ps1
+++ Setup.ps1
@@ -2,40 +2,38 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 
 
 #Utilities
-choco install sumatrapdf.install -y
+choco install foxitreader -y
 choco install firefox -y
 choco install 7zip.install -y
 choco install notepadplusplus.install -y
 choco install cccp -y
 choco install mpc-be -y
 choco install irfanview -y
-choco install mremoteng -y
 choco install nextcloud-client -y
-choco install curl -y
 choco install dropbox -y
 choco install windirstat -y
 choco install greenshot -y
 choco install steelseries-engine -y
 choco install filezilla -y
-choco install windirstat -y
 choco install winscp -y
-choco install tabby -y
+choco install nextcloud-client -y
+choco install localsend -y
+choco install mobaxterm -y
+choco install cheatengine -y
+choco install putty -y
+choco install rpi-imager -y
+choco install razer-synapse-3 -y
+choco install etcher -y
 
 #Programming
 choco install git -y
-choco install github-desktop -y
-choco install visualstudio2019community -y
 choco install vscode -y
 
 #Organisation
-choco install joplin -y
-choco install cherrytree -y
 
 #Gaming
 choco install steam -y
 
-#Requirements
-choco install javaruntime -y
 
 #graphics
 choco install geforce-game-ready-driver -y
@@ -43,4 +41,4 @@ choco install geforce-game-ready-driver -y
 #Comms
 choco install webex -y
 choco install discord -y
-choco install mumble -y
+choco install mumble -yx
